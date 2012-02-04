9d406a9b59f8eef536292ddea913d6e207502455
jst <asterione@gmail.com>
2012-02-04 19:53:08 +0100

    add files

--- p690-vendor.mk
+++ p690-vendor.mk
@@ -20,12 +20,7 @@ PRODUCT_PACKAGES := \
     LiveWallpapersPicker \
     MagicSmokeWallpapers \
     VisualizationWallpapers \
-    librs_jni \
-    FileManager \
-    CMParts \
-    CMScreenshot \
-    CMStats \
-    AndroidTerm 
+    librs_jni
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES := \
